b438b106ae8b08ed6a6c20e087bf692827495077
sayordebbarma <sayor.debbarma2021@vitbhopal.ac.in>
2023-04-03 18:50:18 +0530

    java second commit

--- helloWorld.java
+++ helloWorld.java
@@ -92,5 +92,101 @@ public class helloWorld {
         boolean highSalary = (salary < 600_000);
         System.out.println("high salary earning: " + highSalary);
 
+        //ternary operation
+        int marks = 95;
+        String grades = marks > 90 ? "1st division" : "2nd division";
+        System.out.println(grades);
+
+        //switch statement
+        int roll_no = 5;
+        switch (roll_no) {
+            case 1:
+                System.out.println("Abhi... present ma'am");
+                break;
+            case 2:
+                System.out.println("Breeze... absent ma'am");
+                break;
+            case 3:
+                System.out.println("Candy... absent ma'am");
+                break;
+            case 4:
+                System.out.println("Deelip... absent ma'am");
+                break;
+            case 5:
+                System.out.println("Easter... present ma'am");
+                break;
+            case 6:
+                System.out.println("Fuzi... present ma'am");
+                break;
+            default:
+                System.out.println("All are present");
+        }
+
+        //FizzBuzz exercise
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.print("Number: ");
+        int number1 = scanner2.nextInt();
+
+        if ((number1 % 5 == 0) && (number1 % 3 == 0)) //we have to move the specific code on top
+            System.out.println("FizzBuzz");
+        else if (number1 % 3 == 0)                      // and most generic one on bottom
+            System.out.println("Buzz");
+        else if (number1 % 5 == 0)
+            System.out.println("Fizz");
+        else
+            System.out.println("Nothing happening");
+
+        //for loops
+        for (int i = 0; i < 4; i++) //increment
+            System.out.println("Hi... Robot " + i);
+
+        for (int i = 5; i > 0; i--) //decrement
+            System.out.println("Roll no." + i);
+
+        //while loops
+        int i = 10;
+        while (i > 0) {
+            System.out.println("Boring World!");
+            i--;
+        }
+
+        Scanner scanner3 = new Scanner(System.in);
+        String input = "";
+
+        while (!input.equals("quit")) {  // quit the loop when input is quit
+            System.out.print("Input: ");
+            input = scanner3.next().toLowerCase();
+            System.out.println(input);
+        }
+
+        //do-while loops
+        do {
+            System.out.print("Input: ");
+            input = scanner3.next().toLowerCase();
+            System.out.println(input);
+        } while (!input.equals("quit"));
+
+        //break & continue
+        while (true) {
+            System.out.print("Input: ");
+            input = scanner3.next().toLowerCase();
+            if (input.equals("pass"))
+                continue;
+            if (input.equals("quit"))
+                break;
+            System.out.println(input);
+        }
+
+        //for-each loops
+        String[] vegetables = {"Carrot", "Reddish", "Cabbage", "Cauliflower", "Onion"};
+        
+//        for (int i = 0; i < vegetables.length; i++)
+//            System.out.println(vegetables[i]);
+        
+        for (String veg: vegetables)
+            System.out.println(veg);
+
+        //limitation: we can't iterate from the end of array
+
     }
   }
